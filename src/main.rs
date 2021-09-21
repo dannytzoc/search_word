1e462e72e3f6fc7c7c4f83cf98b708e0b850060b
dannytzoc <tzocdanny@gmail.com>
2021-09-20 20:56:05 -0700

    added if and else

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,17 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() < 3{
+        println!("Not enoough commands");
+        println!("to look at all avaible commands type -help or -h ");
 
-    let query = &args[1];
-    let filename = &args[2];
+    }else {
+        let query = &args[1];
+        let filename = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+        println!("Searching for {}", query);
+        println!("Size of vec {}",args.len());
+        println!("In file {}", filename);
+    }
+
 }
