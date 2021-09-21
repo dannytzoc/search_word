d5750a5b7f36b01e7c41c5d15533398870abc3ef
dannytzoc <tzocdanny@gmail.com>
2021-09-21 13:08:19 -0700

    added c logic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,12 +8,22 @@ fn main() {
         println!("to look at all avaible commands type -help or -h ");
 
     }else {
-        let query = &args[1];
-        let filename = &args[2];
+        let command = &args[1];
+        let query = &args[2];
+        let filename = &args[3];
+        commands(command,query,filename);
 
-        println!("Searching for {}", query);
-        println!("Size of vec {}",args.len());
-        println!("In file {}", filename);
     }
 
+}
+fn word_counter(word:&String ,nm_file:&String){
+    let content = fs::read_to_string(nm_file)
+        .expect("Could not read in file");
+    let wcount = content.matches(word).count();
+    println!("The word appears {} times ",wcount );
+}
+fn commands(c:&String , q: &String, f: &String){
+    if c == "c"{
+        word_counter(q,f);
+    }
 }
