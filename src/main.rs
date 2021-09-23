b429922970ab0e2a8bbc48f92a3591e81d16581d
dannytzoc <tzocdanny@gmail.com>
2021-09-22 23:09:50 -0700

    finsiehd implemtning need to add comments

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3{
         println!("Not enoough commands");
-        println!("to look at all avaible commands type -help or -h ");
+        help();
 
     }else {
         let command = &args[1];
@@ -22,8 +22,17 @@ fn word_counter(word:&String ,nm_file:&String){
     let wcount = content.matches(word).count();
     println!("The word appears {} times ",wcount );
 }
+fn help(){
+    println!("command below currently waiting to add more commands ");
+    println!("-c \t \t \t count the number of time a word appears in a txt file");
+}
 fn commands(c:&String , q: &String, f: &String){
-    if c == "c"{
+    if c == "c" || c=="count"{
         word_counter(q,f);
+    }
+    else if c == "help" || c == "h"{
+        help()
+    }else{
+        println!("Not a commnad made yet");
     }
 }
